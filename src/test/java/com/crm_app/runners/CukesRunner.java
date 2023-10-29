3a4c744833e37259bd82c4e509fa0cc95ba5282c
abdelali20223 <abdelalielgarnaoui1993@gmail.com>
2023-10-28 20:10:00 -0400

    Initial commit

---
+++ src/test/java/com/crm_app/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.crm_app.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/crm_app/step_definitions",
+        dryRun = true,
+        tags = "",
+        publish = true
+)
+public class CukesRunner {}
+
+
---
+++ src/test/java/com/crm_app/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.crm_app.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/crm_app/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
