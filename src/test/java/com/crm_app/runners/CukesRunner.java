3c109f673c09545a248d44eee3d7f5ecd88cc1bb
abdel <abdelazizouberka415@gmail.com>
2023-10-31 14:20:05 -0400

    US84 is done

--- src/test/java/com/crm_app/runners/CukesRunner.java
+++ src/test/java/com/crm_app/runners/CukesRunner.java
@@ -9,12 +9,14 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
+
         },
         features = "src/test/resources/features",
         glue = "com/crm_app/step_definitions",
-        dryRun = true,
-        tags = "",
+        dryRun = false,
+        tags = "@B30G21-101",
         publish = true
 )
 public class CukesRunner {}
